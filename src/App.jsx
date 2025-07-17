0d32426c859e56efd9d7027f797ac646d031bfc6
digitalterp <directorkeith85@gmail.com>
2025-07-17 11:02:36 -0400

    Adds Firestore Tutorial Project

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+
+function App() {
+  const [name, SetName] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPasword] = useState('');
+  const [user, SetUser] = useState(null);
+
+  useEffect(() => {
+    async function testFirestore() {
+      const docRef = doc(db, "testCollection", "testDocument");
+      const docSnap = await getDoc(docRef);
+
+      // Updates Specific Field
+      await updateDoc(docRef, {
+        age: "3500",
+        name: "Leto II"
+      })
+
+      if (docSnap.exists()) {
+        console.log("Updated Document data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    }
+
+    testFirestore();
+  }, []);
+
+
+  // Sign Up
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      SetUser(userCredential.user);
+      console.log('User signed up:', userCredential.user);
+    })
+    .catch(error =>{
+      console.error('Error signing up:', error);
+    });
+  }
+
+  // Sign In
+
+
+  // Sign Out
+  const logOut = () => {
+    signOut(auth)
+    .then( () => {
+      setUser(null);
+      console.log('User is Signed Out');
+    })
+    .catch(error => {
+      console.error('Error signing out:', error);
+    })
+  } 
+
+
+  return (
+    <>
+    <p>Firestore Authentication</p>
+
+    
+    <div>
+      <input type="text" placeholder= 'Email' value={email} onChange={(event) => setEmail(event.target.value)} />
+      <input type="password" placeholder='Password' value={password} />
+      <button onClick={signUp}>Sign Up</button>
+      <button onClick={signIn}>Sign In</button>
+      <button onClick={logOut}>Sign Out</button>
+    </div>
+
+    {
+      user && (
+        <div>
+          <p>Logged in As: {user.email}</p>
+        </div>
+      )
+    }
+    </>
+  )
+}
+
+export default App
